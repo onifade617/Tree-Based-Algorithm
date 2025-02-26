259cc8f0a4d907b8ea22306c320eb1768143218e
Onifade Monsuru <onifade617@gmail.com>
2025-02-26 15:14:27 +0100

    Gini impurity

--- decision_tree.py
+++ decision_tree.py
@@ -6,6 +6,26 @@ Created on Wed Feb 26 14:53:01 2025
 """
 
 import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 st.header("Tree Based Algorithm")
+
+
+st.title("Gini Impurity")
+pos_fraction = np.linspace(0.00, 1.00, 1000)
+
+gini = 1 - (pos_fraction**2 - (1-pos_fraction)**2)
+
+
+fig, ax = plt.subplots()
+
+ax.plot(pos_fraction, gini)
+
+ax.ylim(0, 1)
+ax.xlabel('Positive fraction')
+
+ax.ylabel('Gini impurity')
+
+st.pyplot(fig)
